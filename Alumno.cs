2acd555c5df477906d6ada31c343f8e4d1278916
MaiteRodas <wendyrr28@gmail.com>
2020-04-21 01:41:43 -0300

    first commit

---
+++ Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiteTP1
+{
+    class Class Alumno
+    {
+        public string LegajoA;
+        public string DNIA;
+        Date FInscipcion;
+
+
+
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiteTP1
+{
+    class Usuario
+    {
+        public string DNI;
+        public string Clave;
+        public string LegajoU;
+        public string Mail;
+    }
+}
